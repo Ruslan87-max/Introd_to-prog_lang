26fecb0350b7ee5a99190ecda577acf34ff49645
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-09-27 12:41:59 +0300

    Added lesson:task4

---
+++ LESSON2/task4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите количество гирь: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int max = arr[0];
+int j = 0;
+
+Console.WriteLine("\nЗаполните массив числами: ");
+for(int i = 0; i < arr.Length; i ++) {
+    Console.Write($"arr[{i}] = ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+while(j < arr.Length) {
+    if(arr[j] > max) {
+        max = arr[j];
+    }
+    j ++;
+}
+
+Console.WriteLine($"\nmax of the array is: {max}");
+
+max = arr[0];
+foreach(int e in arr) {
+    if(e > max) {
+        max = e;
+    }
+}
+
+Console.WriteLine($"\nmax of the array is: {max}");
