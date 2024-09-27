6e279be98cd13159dd84f080a9869c12c23199d4
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-09-27 11:07:06 +0300

    Added lesson2:task1

---
+++ LESSON2/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 10;
+arr[2] = 5;
+arr[3] = 8;
+arr[4] = 13;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
+//Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+int[] arr3 = { 1, 2, 3, 4, 5 };
