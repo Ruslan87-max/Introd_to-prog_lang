0db84dcca2f400b6ab776fcc6522a09346b3b06c
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-09-27 12:10:09 +0300

    added lesson:task3

---
+++ LESSON2/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int j = 0;
+
+Console.WriteLine("\nЗаполните массив числами: ");
+for(int i = 0; i < arr.Length; i ++) {
+    Console.Write($"arr[{i}] = ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("\nЧётнеые числа массива: ");
+while(j < arr.Length) {
+    if(arr[j] % 2 == 0) {
+        Console.WriteLine($"arr[{j}] = {arr[j]}");
+    }
+    j ++;
+}
+
