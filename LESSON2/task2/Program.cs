a3673d9b438f42ddce3475b24611feb0aaf4bed5
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-09-27 11:33:11 +0300

    Added lesson2:task2

---
+++ LESSON2/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int i = 0;
+
+while(i < arr.Length) {
+    arr[i] = i + 1;
+    Console.WriteLine($"arr[{i}] = {arr[i]}");
+    i ++;
+}
