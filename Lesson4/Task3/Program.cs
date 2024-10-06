5bc65892b9e559e3bf064acd816a146e31208fd9
Ruslan Mukhametdinov <ruslanm87r@gmail.com>
2024-10-06 19:08:04 +0300

    Add Lesson4:Task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s) {
+        letters = char.IsAsciiLetter(e) == true ? letters + e : letters;
+    }
+    return letters; 
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
